38b8009e5ff72278a137ec1e7cf6eed6a691da85
assia <assiachalh08@gmail.com>
2023-09-12 14:21:13 -0700

    n

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -6,8 +5,17 @@
  *
  *  return: always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	write(1, &c, 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
