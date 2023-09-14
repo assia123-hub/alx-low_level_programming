f8dbe049383e9e67438c39f7fa39a09acdc0f633
assia <assiachalh08@gmail.com>
2023-09-14 13:59:18 -0700

    h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * more_numbers - nmbr fct
  *
- * Return: Always 0.
+ * Return: no return
  */
-int main(void)
+
+void more_numbers(void)
 {
-    more_numbers();
-    return (0);
+
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
