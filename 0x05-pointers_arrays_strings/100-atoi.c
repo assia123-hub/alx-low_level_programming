8f62046d47d193b93c9b78da37f5fe0d24e772d6
assia <assiachalh08@gmail.com>
2023-09-18 16:55:46 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - The number in the string can be preceded by an infinite number of characters
- * @You need to take into account all the - and + signs before the number
- * @If there are no numbers in the string, the function must return 0
- * @You are not allowed to use long
- * @You are not allowed to declare new variables of “type” array
- * @You are not allowed to hard-code special values
- * @We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * return: always 0.
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int _atoi(char *s)
-{
-	int i, sign, numb;
+	int i, d, n, len, f, digit;
 
 	i = 0;
-	sign = 1;
-	numb = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	while (s[i] != '\0')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			while (s[i] >= '0' && s[i] <= '9')
-			{
-				numb = (s[i] - '0') * sign + numb * 10;
-				i++;
-			}
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
-	return (numb);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
