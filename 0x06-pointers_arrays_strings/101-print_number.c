c5e229446ee808512216ff1f5b5107e2f5137de3
assia <assiachalh08@gmail.com>
2023-09-21 15:40:18 -0700

    a

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
- * @n: The integer to print
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
-        if (n == -2147483648)
-        {
-                print_number(-2147483648);
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-                print_number(-n);
-        }
-        else if (n >= 10)
-        {
-                print_number(n / 10);
-                _putchar((n % 10) + '0');
-        }
-        else
-        {
-                _putchar(n + '0');
-        }
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
