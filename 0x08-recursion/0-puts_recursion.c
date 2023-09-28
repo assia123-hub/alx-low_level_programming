da0c0a20cc274235f6390a672cae9268557a4601
assia <assiachalh08@gmail.com>
2023-09-28 14:16:22 -0700

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,9 +12,9 @@ void _puts_recursion(char *s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-        }
-        else
-        {
-	       	_putchar('\n');
-        }
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
