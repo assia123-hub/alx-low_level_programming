5ecba7b3edc3b53f076b65c5ea15d1fe0d13c01c
assia <assiachalh08@gmail.com>
2023-09-28 15:12:48 -0700

    m

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse.
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
@@ -10,9 +10,5 @@ void _print_rev_recursion(char *s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
