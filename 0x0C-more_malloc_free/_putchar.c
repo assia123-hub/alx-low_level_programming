777519a88be58041ca2d7c96a9e97fcb71460bad
assia <assiachalh08@gmail.com>
2023-10-08 06:17:54 -0700

    a

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,12 +1,25 @@
+#include « main.h »
+
 #include <unistd.h>
 
 /**
- * _putchar: writes chaacter c to output 
- * @c: specifies the character to print 
- * Return: 1
+
+ * _putchar - writes the character c to stdout
+
+ * @c: The character to print
+
+ *
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
  */
 
 int _putchar(char c)
+
 {
-	return (write(1, &c, 1))
+
+        return (write(1, &c, 1));
+
 }
