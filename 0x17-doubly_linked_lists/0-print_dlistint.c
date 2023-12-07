a309a7dc67e4bebad511ef4118898c12131d4260
assia <assiachalh08@gmail.com>
2023-12-07 06:09:32 -0800

    python

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,22 @@
 #include "lists.h"
-/**
-* dlistint_len - function
-* @h: pointer to head node of list
-*
-* Description: function to return number of elements in a lnkd list
-* Return: Number of elements
-*/
-size_t dlistint_len(const dlistint_t *h)
-{
-	int elements = 0;
 
-	while (h != NULL)
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ *
+ * @h: the head pointer to the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
 	{
+		printf("%d\n", h->n);
+		count++;
 		h = h->next;
-		elements++;
 	}
-	return (elements);
+	return (count);
 }
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,25 +1,28 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @head: pointer to the first element of the list.
- * @n: int to set in the new node.
- * Return: address of the new element, or NULL if it failed
- **/
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the value of the node to add
+ *
+ * Return: the new pointer
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (*head)
-		(*head)->prev = new;
-	*head = new;
+	if (new)
+	{
+		new->n = n;
+		new->prev = NULL;
+		if (*head)
+			(*head)->prev = new;
+		new->next = *head;
+		*head = new;
+	}
 
 	return (new);
 }
